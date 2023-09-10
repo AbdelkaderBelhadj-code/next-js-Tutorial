7abeee3f4c72e893ce5c5cd7b2ff853f6ec0aafe
AbdelkaderBelhadj-code <belhajabdelkader3@gmail.com>
2023-09-10 18:09:46 +0100

    layout test

---
+++ src/app/acticles/layout.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line react/display-name
+export default function ({children}) {
+    return(
+        <div>
+            <h1>TEST</h1>
+            <div style={{marginTop:"50px" , background:"blue" , padding:"20px" , borderRadius:"10px"}}>
+                {children}
+            </div>
+        </div>
+    )   
+}
