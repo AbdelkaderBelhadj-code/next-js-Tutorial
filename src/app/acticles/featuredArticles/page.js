466f0a683c4c140fe6afe646501922a88acc4c75
AbdelkaderBelhadj-code <belhajabdelkader3@gmail.com>
2023-09-10 17:30:22 +0100

    first

---
+++ src/app/acticles/featuredArticles/page.js
@@ -0,0 +1,9 @@
+const FeatureArticlesPage = () => {
+    return(
+        <div>
+            <h1>Featured Articles</h1>
+        </div>
+    )
+}
+
+export default FeatureArticlesPage;
---
+++ src/app/acticles/page.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+
+export default function ActiclesPage(){
+    return(
+        <div>
+            <h3>Articles Page</h3>
+
+            <Link href="/posts">
+            <button>Take me to the posts page</button>
+            </Link>
+        </div>
+
+       
+    );
+}
